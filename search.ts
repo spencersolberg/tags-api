67d6e70fa807c0738ed326e3de706322970ebb0a
Spencer <you@example.com>
2021-09-05 13:35:02 -0400

    Sorting by downloaded

--- search.ts
+++ search.ts
@@ -1,7 +1,7 @@
 import { parse, json } from "./deps.ts";
 
 export const search =  async (query: string): Promise<Response> => {
-    const req = await fetch(`https://www.barbershoptags.com/api.php?q=${query}`);
+    const req = await fetch(`https://www.barbershoptags.com/api.php?Sortby=Downloaded&q=${query}`);
     return json(parse(await req.text()), {
         headers: {
             "Access-Control-Allow-Origin": "*"
