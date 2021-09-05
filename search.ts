38eee5c9732c1a3ce789e06132747244f36e5b1e
Spencer <you@example.com>
2021-09-05 14:28:07 -0400

    Return more results

--- search.ts
+++ search.ts
@@ -1,7 +1,7 @@
 import { parse, json } from "./deps.ts";
 
 export const search =  async (query: string): Promise<Response> => {
-    const req = await fetch(`https://www.barbershoptags.com/api.php?Sortby=Downloaded&q=${query}`);
+    const req = await fetch(`https://www.barbershoptags.com/api.php?Sortby=Downloadedn=150&q=${query}`);
     return json(parse(await req.text()), {
         headers: {
             "Access-Control-Allow-Origin": "*"
